e61f7fa56ee60d9b0775af8771385a30a22fe282
programstarter <programstarter@gmail.com>
2024-11-16 14:15:21 -0800

    add classes

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import render_template, send_from_directory, request
+import random
 
-from difflib import SequenceMatcher
 def similar(a, b):
+    from difflib import SequenceMatcher
     return SequenceMatcher(None, a.lower(), b.lower()).ratio()
 
 app = Flask(__name__)
@@ -13,11 +14,23 @@ def page1():
 
 schools = ["UCLA", "USC", "CPP"]
 
+def handle_classes():
+    gen_eds = ["English", "Japanese", "Japanese II", "Dance", "Depression"]
+    gen_eds = random.sample(gen_eds, k=len(gen_eds))
+
+    classes = ""
+    for y in range(1, 4+1):
+        classes += f"""
+Year {y}:
+{gen_eds[y%len(gen_eds)]}
+"""
+    return classes
+
 @app.route('/school')
 def hello():
     school_from = request.args.get('from')
     if school_from in schools:
-        return render_template('school.html', school_from=school_from)
+        return render_template('school.html', school_from=school_from, classes=handle_classes())
     else:
         nearest = max(schools, key=lambda r: similar( r.upper(), school_from.upper() ))
         return render_template('school_error.html', school_from=school_from, DYM=nearest)
